79707eb92af8ee676b14f9b42abeaf9d25adbac1
edmondlwk312 <laiwk-wm17@student.tarc.edu.my>
2019-11-07 11:08:27 +0800

    Pracrical_2.4

--- app/src/main/java/com/example/pracrical_24/MainActivity.kt
+++ app/src/main/java/com/example/pracrical_24/MainActivity.kt
@@ -18,8 +18,6 @@ class MainActivity : AppCompatActivity()
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
-
-
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.myName = myName
